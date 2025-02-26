b918fe7a1cf6993b1a9680537434aee5a8c706d5
mnv-godhani <godhanik87@gmail.com>
2025-02-26 23:40:03 +0530

    Feedback

--- frontend/src/component/Feedback.js
+++ frontend/src/component/Feedback.js
@@ -97,7 +97,7 @@ const FeedbackForm = () => {
                 ) : (
                   <form className="feedback-form" onSubmit={handleSubmit}>
                     <center>
-                      <h2 className="form-title mb-3">Write your review</h2>
+                      <h2 className="form-title mb-3">How is your experience ?</h2>
                     </center>
                     {successMsg && <p className="success-msg">{successMsg}</p>}
                     {errorMsg && <p className="error-msg">{errorMsg}</p>}
