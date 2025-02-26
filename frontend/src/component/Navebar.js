f748b6204ef248a54b581d8b69b116544e59f61a
Vatsal2219 <vatsalsutariya9122@gmail.com>
2025-02-26 23:59:38 +0530

    Hello Navbar update

--- frontend/src/component/Navebar.js
+++ frontend/src/component/Navebar.js
@@ -149,7 +149,7 @@ const Navbar = () => {
           onMouseLeave={(e) => (e.target.style.color = styles.navbarLink.color)}
           to="/About"
         >
-          About
+          ABOUT
         </Link>
         <Link
           to="/contact"
@@ -157,7 +157,7 @@ const Navbar = () => {
           onMouseEnter={(e) => (e.target.style.color = styles.navbarLinkHover.color)}
           onMouseLeave={(e) => (e.target.style.color = styles.navbarLink.color)}
         >
-          Contact
+          CONTACT
         </Link>
 
         <Link
